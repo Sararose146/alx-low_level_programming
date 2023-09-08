e4a271e10501a530e411a4ed132900591961e64d
sara rose <you@example.com>
2023-09-08 16:59:31 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include  <stdio.h>
-int mainn(void){
+
+/**
+ * Main -  the starter point
+ *
+ * Description: A C program that  prints with puts function
+ *
+ * Return: always 0 (success)
+*
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	{
+}
 
