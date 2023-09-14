f95893d1466eef8054490891c38904d04892a4e6
sara rose <you@example.com>
2023-09-13 17:43:35 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - main function to print new line
+ * main - main function to print _putchar
  *
  *Return: Always 0 (succses)
 */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_putchar - main function for printing character
+ *
+ *Return: always 0 (success)
+*/
+
+char _putchar(void)
+{
+	return (0);
+}
