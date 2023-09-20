951187819950d8f1395cc5665b9577709d18ce82
sara rose <you@example.com>
2023-09-19 18:42:10 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *void swap_int - A function to swaps the values of two intergrs a and b
+ *
+ * @a: input parameter
+ * @b: input parameter
+ *
+ * return: return a and b
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
