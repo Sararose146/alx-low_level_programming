1c852f29533c4b9b44b662d35bcd45a3a05f3d7a
sara rose <you@example.com>
2023-10-05 03:09:43 +0300

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - main the enty point.
+ * @argc: the number of command line arguments
+ * @argv:is a prameter to contain the elements in array
+ * Return: always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
+
