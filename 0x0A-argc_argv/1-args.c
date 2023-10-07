8828727b01fe03c7e7f7af6f0ab993b776bf3505
sara rose <you@example.com>
2023-10-07 03:53:56 +0300

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main function is the entry point
+ * @argc: input prameter
+ * @argv: input prameter
+ * Return: always 0 (success)
+*/
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
