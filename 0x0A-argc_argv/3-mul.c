ec438bd3ae86e7dc63af2c7ac847d5c528698011
sara rose <you@example.com>
2023-10-07 05:24:39 +0300

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function is to print two multipled numbers
+ * * @argc: input argments
+ * @argv: is an array to store the argments
+ * Return1: 1  (error) if the argc != 3
+ * Return: always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+int num1;
+int num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	 num1 = atoi(argv[1]);
+	 num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+return (0);
+}
